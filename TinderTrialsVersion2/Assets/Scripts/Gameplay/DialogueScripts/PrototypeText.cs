be508a616a0899be8e5adba370f60d689b57363a
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2022-10-27 18:39:43 +0200

    Added introDuction from 3rd year

--- TinderTrialsVersion2/Assets/Scripts/Gameplay/DialogueScripts/PrototypeText.cs
+++ TinderTrialsVersion2/Assets/Scripts/Gameplay/DialogueScripts/PrototypeText.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class PrototypeText : MonoBehaviour
 {
+
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
      void Start()
     {
@@ -14,7 +17,7 @@ public class PrototypeText : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting a convo with " + dialogue.name);
-
+        nameText.text = dialogue.name;
         sentences.Clear();
 
         foreach (string sentence in dialogue.sentences)
@@ -22,15 +25,24 @@ public class PrototypeText : MonoBehaviour
             sentences.Enqueue(sentence);
         }
 
+        displayNextSentence();
         
     }
     public void displayNextSentence()
     {
         if (sentences.Count==0)
         {
-
+            EndDialogue();
+            return;
         }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
     }
+    public void EndDialogue()
+    {
+        Debug.Log("End of convo");
+    }
+
 
 
 }
